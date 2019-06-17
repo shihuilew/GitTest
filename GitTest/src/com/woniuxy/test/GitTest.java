bfc084c36dd22be3a6eb54526fd70dd13c740c7c
shihuiLew <wliushihui@icloud.com>
2019-06-17 15:01:18 +0800

    commit

--- GitTest/src/com/woniuxy/test/GitTest.java
+++ GitTest/src/com/woniuxy/test/GitTest.java
@@ -1,10 +1,15 @@
 package com.woniuxy.test;
 
 public class GitTest {
-public static void main(String[] args) {
-	System.out.println("hello git");
-}
-public void fun(){
-	System.out.println("hello world");
-}
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+
+	public void fun() {
+		System.out.println("hello world");
+	}
+
+	public void fun2() {
+		System.out.println("hello world");
+	}
 }
